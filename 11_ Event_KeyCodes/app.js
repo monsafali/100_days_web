0fb6b08c20519f292ad0330561d3327989c304a2
monsafali <monsafali272@gmail.com>
2024-05-28 16:53:20 +0500

    press key and show their keycode

---
+++ 11_ Event_KeyCodes/app.js
@@ -0,0 +1,24 @@
+const insert = document.getElementById('wrapper');
+
+
+window.addEventListener("keydown",(event)=>{
+    insert.innerHTML = `
+    <div class="key">
+        ${event.key === ' ' ? 'Space' : event.key }
+        <small>event.key</small>
+     </div>
+
+     <div class="key">
+        ${event.keyCode}
+        <small>event.keyCode</small>
+     </div>
+
+     <div class="key">
+        ${event.code}
+        <small>event.code</small>
+     </div>
+     `
+    console.log(event)
+})
+
+
